619b13012c8f2a8635721791a86699aad9c328b2
Luan (Mike) <1703843+mikedark@users.noreply.github.com>
2021-07-06 18:24:29 -0300

    Login Screen e Dependencias

---
+++ App_Farmacia/src/main/java/br/com/apoo2021/farm/screens/LoginScreenController.java
@@ -0,0 +1,14 @@
+package br.com.apoo2021.farm.screens;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginScreenController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
