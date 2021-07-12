d441777f4df7caa918aafaed4cb416ae244928c9
Artur <arturprata@alunos.utfpr.edu.br>
2021-07-12 19:37:01 -0300

    Desenvolvimento Login Screen

--- App_Farmacia/src/main/java/br/com/apoo2021/farm/screens/LoginScreenController.java
+++ App_Farmacia/src/main/java/br/com/apoo2021/farm/screens/LoginScreenController.java
@@ -1,5 +1,10 @@
 package br.com.apoo2021.farm.screens;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 import java.net.URL;
@@ -7,8 +12,31 @@ import java.util.ResourceBundle;
 
 public class LoginScreenController implements Initializable {
 
+    @FXML
+    private JFXTextField usernameTextField;
+
+    @FXML
+    private JFXPasswordField passwordTextField;
+
+    @FXML
+    private JFXButton loginButton;
+
+    @FXML
+    private JFXButton signUpButton;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
     }
+
+    @FXML
+    void loginPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void signUpPressed(ActionEvent event) {
+
+    }
+
 }
